f6085e20109614d042a77d936ab384f74f27d57d
Manohar752 <manoharbathula912@gmail.com>
2023-12-29 16:18:08 +0530

    init

--- test.js
+++ test.js
@@ -1,48 +1,48 @@
-// This program is written by Manohar// 
+// // This program is written by Manohar// 
 
-let x=function (n1,n2){
-    let sum = n1+n2
-    return sum
-}
-let i = x(10,20)
-document.write(i)
-
-
+// let x=function (n1,n2){
+//     let sum = n1+n2
+//     return sum
+// }
+// let i = x(10,20)
+// document.write(i)
 
 
-// This program is written by Manohar// 
 
-function isPrime(num)
-{
-    let value=0
-    let i=1
 
-    for (i=1;i<=num;i++)
-    {
-        if(num % i === 0)
-        {
-            value++
-        }
+// // This program is written by Manohar// 
+
+// function isPrime(num)
+// {
+//     let value=0
+//     let i=1
+
+//     for (i=1;i<=num;i++)
+//     {
+//         if(num % i === 0)
+//         {
+//             value++
+//         }
         
-    }
-    if (value===2)
-    {
-        return num
-    }
-}  
-    function filter( numbers,fn)
-    {
-        let res=[]
-        for( let x in numbers)
-        {
-            if (fn(x))
-            {
-                res.push(x)
-            }
-        }
+//     }
+//     if (value===2)
+//     {
+//         return num
+//     }
+// }  
+//     function filter( numbers,fn)
+//     {
+//         let res=[]
+//         for( let x in numbers)
+//         {
+//             if (fn(x))
+//             {
+//                 res.push(x)
+//             }
+//         }
     
-    return res
-    }
+//     return res
+//     }
 
 
 
@@ -50,18 +50,54 @@ function isPrime(num)
 
 
 
-const numbers = [ 11,20,3,4,50,6,7,8,9,20]
+// const numbers = [ 11,20,3,4,50,6,7,5,8,9,20]
 
-let evenArr = []
-
-   let isEven = ((num)=>
-   {
-      return num % 2 === 0
-   }
-   )
-   
-   
-
-newArr = numbers.filter(isEven)
+//     isEven = (num)=>
+//     {
+//          return num % 2 === 0
+//     }
+// newArr = numbers.filter(isEven)
  
-console.log(newArr)
+// console.log(newArr)
+
+
+// function num (...numbers){
+
+// let sum = 0
+// let i
+
+// for(i of numbers){
+//     sum = sum + i
+// } 
+//  return sum
+// }
+
+// res=num( 11,22,30,40,50)
+
+// console.log(res)
+
+
+// const numbers = [440,20,30,40,50]
+
+// const sum = numbers.sort ((accumalator,currentvalue)=>  accumalator + currentvalue )
+
+
+// console.log(sum)
+
+
+// let numb = [2,4,6,8,10]
+    
+//  isEven=((num)=> num % 2 === 0)
+
+//  const res = numb.every(isEven)
+//  console.log(res)
+
+// OBJECTS*******
+let alien = {
+    name:'manu',
+    technology:'js'
+}
+console.log(alien);
+
+
+
